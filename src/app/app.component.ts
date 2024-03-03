7d4b86b22e59f2de7d773b299cbddef087d7506e
Melvin Sibichen <melvinsibichen1020@gmail.com>
2024-03-03 22:51:39 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AngularEditorConfig } from '@kolkov/angular-editor';
+// @ts-ignore
+import html2pdf from 'html2pdf.js';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'kolkov';
+  htmlContent = '';
+
+  config: AngularEditorConfig = {
+    editable: true,
+    spellcheck: true,
+    height: '15rem',
+    minHeight: '5rem',
+    placeholder: 'Enter text here...',
+    translate: 'no',
+    defaultParagraphSeparator: 'p',
+    defaultFontName: 'Arial',
+  };
+
+  @ViewChild('contentToConvert') contentToConvert!: ElementRef;
+
+  exportAsPDF() {
+    const element = this.contentToConvert.nativeElement;
+    const opt = {
+      margin: 0.5,
+      filename: 'myfile.pdf',
+      image: { type: 'jpeg', quality: 0.98 },
+      html2canvas: { scale: 2 },
+      jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+    };
+
+    // New Promise-based usage:
+    html2pdf().from(element).set(opt).save();
+  }
 }
